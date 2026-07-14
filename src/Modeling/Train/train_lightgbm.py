cc9613e272b303a5eca58550740939c4aa5e4db8
Amany-Ibrahim-1510 <amany.ibrahim850@gmail.com>
2026-07-14 15:29:56 +0300

    changes

---
+++ src/Modeling/Train/train_lightgbm.py
@@ -0,0 +1,102 @@
+
+import os
+import sys
+
+sys.path.append(
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            '../..'
+        )
+    )
+)
+
+from Config.load import load_config
+
+from Modeling.Helper.prepare import Preparing
+from Modeling.Helper.eval import eval_model
+from Modeling.Helper.save import save_model
+
+from lightgbm import LGBMClassifier
+
+
+config = load_config()
+
+
+class Train():
+
+    def __init__(
+        self,
+        x_train,
+        x_val,
+        t_train,
+        t_val
+    ):
+
+        self.x_train = x_train
+        self.x_val = x_val
+
+        self.t_train = t_train
+        self.t_val = t_val
+
+    def try_lightgbm(self):
+
+        params = config["model"]["lightgbm"]["params"]
+
+        model = LGBMClassifier(
+            max_depth=params["max_depth"],
+            learning_rate=params["learning_rate"],
+            n_estimators=params["n_estimators"],
+            random_state=params["random_state"]
+        )
+
+        model.fit(
+            self.x_train,
+            self.t_train
+        )
+
+        print("\nTrain Results")
+
+        eval_model(
+            model,
+            self.x_train,
+            self.t_train,
+            "train"
+        )
+
+        print("\nValidation Results")
+
+        eval_model(
+            model,
+            self.x_val,
+            self.t_val,
+            "val"
+        )
+
+        return model
+
+
+if __name__ == "__main__":
+
+    prepare = Preparing()
+
+    x_train, x_val, t_train, t_val, scaler = (
+        prepare.prepare_data()
+    )
+
+    train = Train(
+        x_train,
+        x_val,
+        t_train,
+        t_val
+    )
+
+    model = train.try_lightgbm()
+
+    save_model(
+        model,
+        scaler,
+        "lightgbm"
+    )
+
+    print("\nSuccessful")
